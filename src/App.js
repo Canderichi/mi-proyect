3548d5994da1de561bee0ac07e870e35501ac37e
Candela Richiardi <63475338+Canderichi@users.noreply.github.com>
2020-08-07 21:13:58 -0300

    correcciones

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import {CSSTransition} from 'react-transition-group';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <NavBar>
+      <NavItem icon="😊"></NavItem>
+      <NavItem icon="😍"></NavItem>
+      <NavItem icon="😉"></NavItem>
+      <NavItem icon="🤪">
+        <DropdownMenu></DropdownMenu>
+
+
+      </NavItem>
+
+
+    </NavBar>
+  );
+}
+
+function DropdownMenu() {
+
+  const [activeMenu, setActiveMenu] = useState('main');
+  
+
+  function DropdownItem(props) {
+    return (
+      <a href="#" className="menu-item" onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}>
+        <span className="icon-button">{props.leftIcon}</span>
+
+        {props.children}
+
+        <span className="icon-right">{props.rightIcon}</span>
+      </a>
+    );
+  }
+  return (
+    <div className="dropdown">
+      <CSSTransition 
+      in={activeMenu === 'main'} 
+      unmountOnExit 
+      timeout={500}
+      classNames="menu-primary"
+      >
+        <div className="menu">
+        <DropdownItem>My profile</DropdownItem>
+        <DropdownItem
+        leftIcon="👩‍💻"
+        rightIcon="🤓"
+        goToMenu="settings"> 
+          Settings
+        </DropdownItem>
+        </div>
+      </CSSTransition>
+
+      <CSSTransition 
+        in={activeMenu === 'settings'} 
+        unmountOnExit 
+        timeout={500}
+        classNames="menu-secondary"
         >
-          Learn React
-        </a>
-      </header>
+          <div className="menu">
+            <DropdownItem leftIcon="👈" goToMenu="main"></DropdownItem>
+            <DropdownItem>Settings</DropdownItem>
+            
+          </div>
+      </CSSTransition>
     </div>
+  );
+}
+
+function NavBar(props) {
+  return (
+    <nav className="navbar">
+      <ul className="navbar-nav">{ props.children }</ul>
+    </nav>
+  );
+}
+
+function NavItem(props) {
+  const[open, setOpen] = useState(false);
+  return (
+    <li className="nav-item">
+      <a href="#" className="icon-button" onClick={() => setOpen(!open)}>
+        {props.icon}
+      </a>
+
+      {open && props.children}
+    </li>
   );
 }
 
